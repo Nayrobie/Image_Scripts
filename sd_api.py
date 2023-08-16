72a6b1dd6d35910ea7ee668432289adab3d96da2
Yonah Bole <80701265+Nayrobie@users.noreply.github.com>
2023-08-16 15:00:41 +0200

    Textinfo on the image name

--- sd_api.py
+++ sd_api.py
@@ -2,13 +2,16 @@ import json
 import requests
 import io
 import base64
+import re
 from PIL import Image
 
 url = "http://127.0.0.1:7860"
 
 payload = {
-    "prompt": "mountain",
+    "prompt": "black dragon",
     "steps": 10,
+    "negative_prompt": "purple",
+    "seed": 475849700
 }
 
 response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
@@ -19,23 +22,17 @@ print(r.get("info"))
 
 info_json = json.loads(r.get("info"))  # Parse the "info" JSON string
 
-# Extract the Seed value from the infotexts list
-seed = None
-for infotext in info_json.get("infotexts", []):
-    if "Seed" in infotext:
-        seed = infotext.split("Seed: ")[1].split(",")[0]
-        break
+# Extract the infotexts value
+infotexts = info_json.get("infotexts")[0]  # Assuming there's only one infotext
 
-for i in r['images']:
-    image_data = base64.b64decode(i.split(",", 1)[0])
-    image = Image.open(io.BytesIO(image_data))
+filename = re.sub(r'[:,\s]+', '_', infotexts)
+filename = filename.replace("__", "_")  # Replace double underscores with a single underscore
+filename = filename + ".jpg"
 
-    # Update the output directory path with the seed value in the filename
-    output_path = f"E:\\GIT_ROOT\\AC-EKO-IA\\AUTOMATIC1111\\outputs\\sd_api\\{seed}.jpg"
-    image.save(output_path, format='JPEG', quality=95)
+# Select the first image from the images list
+image_data = base64.b64decode(r['images'][0].split(",", 1)[0])
+image = Image.open(io.BytesIO(image_data))
 
-    # Open the saved image and add the parameters info as a comment
-    with Image.open(output_path) as saved_image:
-        comment = r.get("info")  # Use the info from the main response
-        saved_image.info["comment"] = comment
-        saved_image.save(output_path)
+# Update the output directory path with the generated filename
+output_path = f"E:\\GIT_ROOT\\AC-EKO-IA\\AUTOMATIC1111\\outputs\\sd_api\\{filename}"
+image.save(output_path, format='JPEG', quality=95)

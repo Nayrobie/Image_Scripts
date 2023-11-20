e23c84825076442bcee85e900f86052592583630
Nayrobie <boleyonah@gmail.com>
2023-11-20 14:22:38 +0100

    added controlnet module for the ui

--- SD_image_generation_A1111.py
+++ SD_image_generation_A1111.py
@@ -8,49 +8,57 @@ an image as input in addition to the prompt. Users can choose between lineart, s
 It allows users to quickly experiment with different generation methods and prompts without the need
 to run the automatic1111 webui manually and it greatly reduces the amount of parameters needed.
 """
+# Documentation:
+# Controlnet API doc https://github.com/Mikubill/sd-webui-controlnet/wiki/API
+# API get info http://127.0.0.1:7860/docs#/default
 
-import io
 import cv2
 import base64
 import requests
-from PIL import Image
-import json
 import tkinter as tk
 from tkinter import simpledialog
 
+# A1111 URL
+url = "http://127.0.0.1:7860"
+
 # User configurations
 input_image_path = r"D:\GIT\house_gray.jpg"
-user_choice = "txt2img"
-prompt = "An old, weathered lighthouse standing tall on a desolate, rocky coastline"
 negative_prompt = "low-quality, watermark, deformed, distorted anatomy, poor artistry, NSFW, Cleavage, Nudity, Naked, explicit content, mutated features, extra limbs, extra fingers, ugly, missing body parts, disconnected elements, malformed, abnormal proportions, aberrant hands, aberrant feet, aberrant  legs, aberrant  fingers"
-model_checkpoint = "juggernautXL_version45.safetensors [ca4802bc3f]"
-controlnet_module = [
-    "lineart",
-    #"scribble_pidinet",
-    #"canny",
-    #"depth_midas"
-    ]
+model_checkpoint = "dreamshaper_8.safetensors [879db523c3]"
 denoising_strengh = 0.3
+
 styles = [
     "3D Rendering",
-    #"Cinematic",
-    #"Portrait",
+    "Cinematic",
+    "Portrait",
     "Digital Drawing",
-    #"Sketchbook",
-    #"Manga",
-    #"Indie Game",
-    #"Craft Clay",
+    "Sketchbook",
+    "Manga",
+    "Indie Game",
+    "Craft Clay",
     "Isometric",
     "Low Poly",
     "Origami",
-    #"Biomechanical",
+    "Biomechanical",
     "Steampunk",
-    #"Pirate Punk",
+    "Pirate Punk",
     "Neon Punk" 
 ]
-
-# A1111 URL
-url = "http://127.0.0.1:7860"
+controlnet_modules = [
+    "canny",
+    "depth_midas",
+    "lineart",
+    "openpose_full",
+    "scribble_pidinet",
+    ]
+# Maps the controlnet modules to the right models
+controlnet_mapping = {
+    "canny": "control_v11p_sd15_canny [d14c016b]",
+    "depth_midas": "control_v11f1p_sd15_depth [cfd03158]",
+    "lineart": "control_v11p_sd15_lineart [43d4be0d]",
+    "openpose_full": "control_v11p_sd15_openpose [cab727d4]",
+    "scribble_pidinet": "control_v11p_sd15_scribble [d4ba51ff]",
+}
 
 def encode_image_to_base64(input_path):
     """
@@ -72,32 +80,132 @@ def encode_image_to_base64(input_path):
     except Exception as e:
         return str(e)
 
-# Create a tkinter window
+# Fast interface, create a window
 root = tk.Tk()
+
+# Function to create controlnet module buttons
+def create_controlnet_buttons():
+    for module in controlnet_modules:
+        module_button = tk.Button(root, text=module, command=lambda m=module: set_controlnet_module(m))
+        module_button.pack()
 
 # Function to set user choice
 def set_user_choice(choice):
     global user_choice
     user_choice = choice
+    if choice == "3":
+        # Clear the existing buttons
+        txt2img_button.pack_forget()
+        img2img_button.pack_forget()
+        controlnet_button.pack_forget()
+        create_controlnet_buttons()
+    else:
+        root.destroy()
+
+# Function to set the selected controlnet module
+def set_controlnet_module(module):
+    global controlnet_module
+    controlnet_module = module
     root.destroy()
 
 # Create buttons for user choice
-controlnet_button = tk.Button(root, text="Controlnet", command=lambda: set_user_choice("3"))
-img2img_button = tk.Button(root, text="Img2img", command=lambda: set_user_choice("2"))
 txt2img_button = tk.Button(root, text="Txt2img", command=lambda: set_user_choice("1"))
+img2img_button = tk.Button(root, text="Img2img", command=lambda: set_user_choice("2"))
+controlnet_button = tk.Button(root, text="Controlnet", command=lambda: set_user_choice("3"))
 
-# Pack buttons
-controlnet_button.pack()
-img2img_button.pack()
+# Create buttons
 txt2img_button.pack()
+img2img_button.pack()
+controlnet_button.pack()
 
 # Wait for the user to click a button
 root.mainloop()
 
+# Asks the user for prompt 
 def get_user_prompt():
     return simpledialog.askstring("User Prompt", "Enter your prompt:")
-
 prompt = get_user_prompt()
+
+# Asks the user for styles 
+#def get_user_styles():
+#    return simpledialog.askstring("User Styles", "Enter styles separated by commas:")
+#styles_input = get_user_styles()
+#styles = styles_input.split(",") if styles_input else []
+
+# Asks the user for controlnet module
+def set_controlnet_module(module):
+    global controlnet_module
+    controlnet_module = module
+    root.destroy()
+
+def txt2img(prompt, negative_prompt, model_checkpoint, styles):
+    txt2img_payload = {
+        "prompt": prompt,
+        "negative_prompt": negative_prompt,
+        "seed": -1,
+        "width": 512,
+        "height": 512,
+        "sampler_name": "Euler a",
+        "cfg_scale": 7.0, 
+        "steps": 30, 
+        "restore_faces": False, 
+        "denoising_strength": 0, 
+        "extra_generation_params": {},
+        "styles": styles,
+        "save_images": True
+    }
+
+    # For the script to override the model chosen on A1111    
+    override_settings = {
+        "sd_model_checkpoint": model_checkpoint
+    }
+    override_payload = {
+        "override_settings": override_settings
+    }
+    txt2img_payload.update(override_payload)
+
+    txt2img_response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=txt2img_payload)
+    r = txt2img_response.json()
+
+def img2img(prompt, negative_prompt, model_checkpoint, input_img, denoising_strengh):
+    """
+    Running Stable Diffusion by providing both a prompt and an initial image. 
+    The denoising strengh is the pourcentage of noise added to the initial image,
+    a value of 1 will completely replace your input image with noise.
+    """
+    # Generation parameters
+    img2img_payload = {
+        "init_images": [input_img],
+        "resize_mode": 0,
+        "image_cfg_scale": 0,
+        "mask_blur": 0,
+        "save_images": True,
+
+        "prompt": prompt,
+        "negative_prompt": negative_prompt,
+        "denoising_strength": denoising_strengh,
+        "width": 512,
+        "height": 512,
+        "cfg_scale": 7,
+        "styles": styles,
+        "sampler_name": "Euler a",
+        "steps": 20
+    }
+
+    # For the script to override the model chosen on A1111    
+    override_settings = {
+        "sd_model_checkpoint": model_checkpoint
+    }
+    override_payload = {
+        "override_settings": override_settings
+    }
+    img2img_payload.update(override_payload)
+
+    img2img_response = requests.post(url=f'{url}/sdapi/v1/img2img', json=img2img_payload)
+    r = img2img_response.json()
+
+    #result = r['images'][0]
+    #image = Image.open(io.BytesIO(base64.b64decode(result.split(",", 1)[0])))
 
 def txt2img_controlnet(prompt, negative_prompt, model_checkpoint, controlnet_module):
     """
@@ -126,6 +234,7 @@ def txt2img_controlnet(prompt, negative_prompt, model_checkpoint, controlnet_mod
                         "weight": 2,
                         "width": 512,
                         "height": 512,
+                        "styles": styles,
                         "resize_mode": "Scale to Fit (Inner Fit)",
                         "control_mode": "Balanced",
                         "pixel_perfect": True
@@ -149,104 +258,21 @@ def txt2img_controlnet(prompt, negative_prompt, model_checkpoint, controlnet_mod
     r = response.json()
     print(f"Generated image for controlnet_module: {controlnet_module}")
 
-def img2img(prompt, negative_prompt, model_checkpoint, input_img, denoising_strengh):
-    """
-    Running Stable Diffusion by providing both a prompt and an initial image. 
-    The denoising strengh is the pourcentage of noise added to the initial image,
-    a value of 1 will completely replace your input image with noise.
-    """
-    # Generation parameters
-    img2img_payload = {
-        "init_images": [input_img],
-        "resize_mode": 0,
-        "image_cfg_scale": 0,
-        "mask_blur": 0,
-        "save_images": True,
 
-        "prompt": prompt,
-        "negative_prompt": negative_prompt,
-        "denoising_strength": denoising_strengh,
-        "width": 512,
-        "height": 512,
-        "cfg_scale": 7,
-        "sampler_name": "Euler a",
-        "steps": 20
-    }
-
-    # For the script to override the model chosen on A1111    
-    override_settings = {
-        "sd_model_checkpoint": model_checkpoint
-    }
-    override_payload = {
-        "override_settings": override_settings
-    }
-    img2img_payload.update(override_payload)
-
-    img2img_response = requests.post(url=f'{url}/sdapi/v1/img2img', json=img2img_payload)
-    r = img2img_response.json()
-
-    #result = r['images'][0]
-    #image = Image.open(io.BytesIO(base64.b64decode(result.split(",", 1)[0])))
-
-def txt2img(prompt, negative_prompt, model_checkpoint, styles):
-    txt2img_payload = {
-        "prompt": prompt,
-        "negative_prompt": negative_prompt,
-        "seed": -1,
-        "width": 1024,
-        "height": 1024,
-        "sampler_name": "Euler a",
-        "cfg_scale": 7.0, 
-        "steps": 30, 
-        "restore_faces": False, 
-        "denoising_strength": 0, 
-        "extra_generation_params": {},
-        "styles": styles,
-        "save_images": True
-    }
-
-    # For the script to override the model chosen on A1111    
-    override_settings = {
-        "sd_model_checkpoint": model_checkpoint
-    }
-    override_payload = {
-        "override_settings": override_settings
-    }
-    txt2img_payload.update(override_payload)
-
-    txt2img_response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=txt2img_payload)
-    r = txt2img_response.json()
-
-# Controlnet module-to-model mapping
-controlnet_mapping = {
-    "lineart": "control_v11p_sd15_lineart [43d4be0d]",
-    "scribble_pidinet": "control_v11p_sd15_scribble [d4ba51ff]",
-    "canny": "control_v11p_sd15_canny [d14c016b]",
-    "depth_midas": "control_v11f1p_sd15_depth [cfd03158]",
-    "openpose_full": "control_v11p_sd15_openpose [cab727d4]",
-}
-
-# Documentation:
-# Controlnet API doc https://github.com/Mikubill/sd-webui-controlnet/wiki/API
-# API get info http://127.0.0.1:7860/docs#/default
 
 encoded_image = encode_image_to_base64(input_image_path)
 
 # User's choice between the different payloads
-if user_choice == "3":
-    if isinstance(controlnet_module, list):
-        # Generate images for each controlnet module
-        for module in controlnet_module:
-            txt2img_controlnet(prompt, negative_prompt, model_checkpoint, module)
-    else:
-        # Send the ControlNet request for a single module
-        txt2img_controlnet(prompt, negative_prompt, model_checkpoint, controlnet_module)
-elif user_choice == "2":
-    # Send the img2img request
-    img2img(prompt, negative_prompt, encoded_image)
-elif user_choice == "1":
+if user_choice == "1":
     # Generate images for each style in the 'styles' list
-    for style in styles:
-        txt2img(prompt, negative_prompt, model_checkpoint, [style])
+    txt2img(prompt, negative_prompt, model_checkpoint)
+elif user_choice == "2":
+    # Generate images for each style in the 'styles' list
+    img2img(prompt, negative_prompt, encoded_image)
+elif user_choice == "3":
+    if controlnet_module in controlnet_modules:
+        txt2img_controlnet(prompt, negative_prompt, model_checkpoint, controlnet_module)
+    else:
+        print("Invalid ControlNet module selected. Please choose from the available modules.")
 else:
     print("Invalid user choice. Please specify '1', '2', or '3'.")

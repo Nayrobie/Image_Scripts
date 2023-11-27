58af83a0b365bd177faecba3c8561a99c2fd9f74
Nayrobie <boleyonah@gmail.com>
2023-11-27 17:13:15 +0100

    ui base with hidden default neg and extra tags

--- SD_ui_A1111.py
+++ SD_ui_A1111.py
@@ -1,35 +1,40 @@
+# python .\artist_ui.py
+
 import gradio as gr
 
-def generate_image(prompt, styles, perspectives, media, extra_tags, negative_prompt):
-    # Generated image logic here
-    return f"Generated image with: {prompt}, {styles}, {perspectives}, {media}, {extra_tags}, {negative_prompt}"
+# Default inputs
+default_extra_tags = ["highly detailed", "masterpiece", "8k", "uhd"]
+default_negative_prompt = ["low-quality, watermark, deformed, distorted anatomy, poor artistry, NSFW, Cleavage, Nudity, Naked, explicit content, mutated features, extra limbs, extra fingers, ugly, missing body parts, disconnected elements, malformed, abnormal proportions, aberrant hands, aberrant feet, aberrant legs, aberrant fingers"]
 
-# Define keyword lists
+def generate_image(prompt, styles, perspectives, media, extra_tags, extra_negative_prompt=default_negative_prompt):
+    # Concatenate additional negative keywords provided by the user with default negative prompt
+    all_negative_prompts = f"{extra_negative_prompt}" if extra_negative_prompt else default_negative_prompt
+    
+    # Generated image logic here, using extra_tags and negative_prompt
+    return f"Generated image with: {prompt}, {styles}, {perspectives}, {media}, {default_extra_tags}, {all_negative_prompts}"
+
+# Define prompt keywords list
 style_list = ["hyper-realistic", "minimalist", "surreal", "abstract", "steampunk", "cyberpunk", "mystical", "moody", "intricate details", "hazy atmosphere", "handcrafted", "mysterious", "ethereal", "enigmatic", "otherworldly"]
 perspective_list = ["depth of field", "fisheye lens", "low view camera angle", "close-up shot", "wide shot", "full body length", "bird’s-eye view", "panoramic", "fixed focal length", "macro", "anamorphic", "overhead", "aerial"]
 media_list = ["sketchbook", "oil painting", "photography", "concept art", "portrait", "3d rendering", "octane render", "editorial cinematic", "cinemascope", "vector depiction", "digital schematics", "pbr material", "watercolour", "charcoal drawing", "graffiti"]
-extra_tag_list = ["highly detailed", "high budget", "epic", "masterpiece", "8k", "photoreal", "inspirational", "narrative-based visual", "uhd image", "dslr effect"]
 
-# Create the input components with Gradio
+# Create the entries with Gradio
 prompt_input = gr.components.Textbox(lines=2, placeholder="Enter your prompt here", label="Prompt")
-style_input = gr.components.CheckboxGroup(choices=style_list, label="Style")
-perspective_input = gr.components.CheckboxGroup(choices=perspective_list, label="Perspective")
-media_input = gr.components.CheckboxGroup(choices=media_list, label="Media")
-extra_tag_input = gr.components.CheckboxGroup(choices=extra_tag_list, label="Extra tag")
-extra_tag_input.set_properties(default=["highly detailed", "masterpiece", "8k"])  # Set default values
-negative_prompt_input = gr.components.Textbox(lines=2, placeholder="Negative prompt", label="Negative prompt")
+style_input = gr.components.Dropdown(choices=style_list, label="Style")
+perspective_input = gr.components.Dropdown(choices=perspective_list, label="Perspective")
+media_input = gr.components.Dropdown(choices=media_list, label="Media")
+extra_negative_prompt_input = gr.components.Textbox(lines=2, placeholder="Enter your negative prompt here", label="Negative prompt")
 
-# Create the Gradio interface
+# Create the ui
 iface = gr.Interface(
     fn=generate_image,
     inputs=[
-        prompt_input, style_input, perspective_input, media_input,
-        extra_tag_input, negative_prompt_input
+        prompt_input, style_input, perspective_input, media_input, extra_negative_prompt_input
     ],
     outputs=gr.components.Text(label="Generated Image Details"),
     title="Stable Diffusion Image Generator",
     description="Fill out the fields below to generate an image."
 )
 
-# Run the interface
+# Run the ui
 iface.launch()
